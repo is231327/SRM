9a84bcb2ead8646ee4ea005adf26fb3ad0ab29fc
is231327 <is231327@ustp-students.at>
2026-09-02 08:53:11 +0200

    feat nothing

--- SRMAuth/Configuration/LoginSecurityOptions.cs
+++ SRMAuth/Configuration/LoginSecurityOptions.cs
@@ -5,4 +5,5 @@ public class LoginSecurityOptions
     public const string SectionName = "LoginSecurity";
     public int MaximumFailures { get; set; } = 5;
     public int FailureWindowMinutes { get; set; } = 15;
+
 }
